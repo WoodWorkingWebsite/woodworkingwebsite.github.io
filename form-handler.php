e07750844a42c79e0871b57fc13bb1d9c9dbe579
Madolyn <34144339+madolyn02@users.noreply.github.com>
2020-07-28 13:15:31 -0400

    form test

--- form-handler.php
+++ form-handler.php
@@ -1,25 +1,17 @@
 <?php
+require('phpmailer/class.phpmailer.php');
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+	$content = $_POST["content"];
 
-  $name = $_POST['name'];
-  $visitor_email = $_POST['email'];
-  $message = $_POST['message'];
-
-  $email_from = 'madolynphotography@gmail.com';
-
-  $email_subject = "Custom Order Request";
-
-  $email_body = "Name: $name.\n".
-                "Email: $visitor_email.\n".
-                "Order Details: $message.\n";
-
-  $to = "21glocksinem@e-hps.net";
-
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply To: $visitor_email \r\n";
-
-  mail($to, $email_subject, $email_body, $headers);
-
-  header("Location: custom.html");
-
-  ?>
+	$toEmail = "21glocksinem@e-hps.net";
+	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
+	if(mail($toEmail, $subject, $content, $mailHeaders)) {
+	    $message = "Your contact information is received successfully.";
+	    $type = "success";
+	}
+}
+require_once "custom.html";
+?>
